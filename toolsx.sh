1afec354e6da33231bcd54a21ce6cfa245313dbe
DrCyber099 <57848832+DrCyber099@users.noreply.github.com>
2019-11-22 18:38:44 +0800

    Add files via upload

---
+++ toolsx.sh
@@ -0,0 +1,722 @@
+#Apa liat² Mau recode
+
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+
+clear
+
+figlet -f slant Tools Termux| lolcat
+
+echo "(+)====================================================(+)" | lolcat
+echo " |                                                      |" | lolcat
+echo " |                Author    :  DevilRangga              |" | lolcat
+echo " |                Team      :  DrCyberTeam              |" | lolcat
+echo " |                Contac    :  089899563xx              |" | lolcat
+echo " |     GUNAKAN TOOLS DENGAN BIJAK HARGAI YG  MEMBUAT    |" | lolcat
+echo " |                                                      |" | lolcat
+echo "(+)====================================================(+)" | lolcat
+date |lolcat
+
+
+echo
+echo "============================" | lolcat
+echo "1. Installer BajinganV6"
+echo "============================" | lolcat
+echo "2. Installer Tools Tu4nG4lau"
+echo "============================" | lolcat
+echo "3. B-Virus"
+echo "============================" | lolcat
+echo "4. RED_HAWK"
+echo "============================" | lolcat
+echo "5. HackFb"
+echo "============================" | lolcat
+echo "6. 57ToolsTermux"
+echo "============================" | lolcat
+echo "7. HackIG"
+echo "============================" | lolcat
+echo "8. PelacakOrang"
+echo "============================" | lolcat
+echo "9. DdosAttack"
+echo "============================" | lolcat
+echo "10.Phising"
+echo "============================" | lolcat
+echo "11.HackCCTV"
+echo "============================" | lolcat
+echo "12.Mengaktifkan PerisaiFTO.FB"
+echo "=============================" | lolcat
+echo "13.SpamCALL"
+echo "============================" | lolcat
+echo "14.HackKAMERA"
+echo "============================" | lolcat
+echo "15.Install Metasploit"
+echo "============================" | lolcat
+echo "16.Install Ko-dork"
+echo "======================================" | lolcat
+echo "17.Milik Pribadi Jangan Instal(Bahaya)"
+echo "======================================" | lolcat
+echo "18.HackGmail"
+echo "============================" | lolcat
+echo "19.Install Linux"
+echo "============================" | lolcat
+echo "20.DarkFB"
+echo "============================" | lolcat
+echo "21.Install Ubuntu"
+echo "============================" | lolcat
+echo "22.Menampilkan Semua Tombol"
+echo "============================" | lolcat
+echo "23.Tembak Paket Internet XL"
+echo "============================" | lolcat
+echo "24.Tembak Paket Kartu 3"
+echo "============================" | lolcat
+echo "25.Nick Anggota Dr.CyberTeam"
+echo "============================" | lolcat
+echo "26.Membuat Script Defece"
+echo "============================" | lolcat
+echo "27.Buat Virus WhatsApp"
+echo "============================" | lolcat
+echo "28.Bot Jones (New)"
+echo "============================" | lolcat
+echo "29.Chat (Admin)"
+echo "============================" | lolcat
+echo "30.Join Grup (WhatsApp)"
+echo "============================" | lolcat
+echo "31.MBF Tools"
+echo "============================" | lolcat
+echo "32.Tools MR-CROOT"
+echo "============================" | lolcat
+echo "33.Info bmkg"
+echo "============================" | lolcat
+echo "34.Install ngrok"
+echo "============================" | lolcat
+echo "35.Santet-Online"
+echo "============================" | lolcat
+echo "36.Osif"
+echo "============================" | lolcat
+echo "37.Wifi-hacker"
+echo "============================" | lolcat
+echo "38.Lazymux"
+echo "============================" | lolcat
+echo "39.Whatshack"
+echo "============================" | lolcat
+echo "40.Katolin"
+echo "============================" | lolcat
+echo "41.VulnScaner"
+echo "============================" | lolcat
+echo "42.Fedora"
+echo "============================" | lolcat
+echo "43.IPGeolocation"
+echo "============================" | lolcat
+echo "44.Sqlmap"
+echo "============================" | lolcat
+echo "45.Tools Diejoubu"
+echo "============================" | lolcat
+echo "46.Bot FB"
+echo "============================" | lolcat
+echo "47.CreateVirus"
+echo "============================" | lolcat
+echo "48.PemulungBTC"
+echo "============================" | lolcat
+echo "49.Phising Game"
+echo "============================" | lolcat
+echo "50.AdminFinder"
+echo "============================" | lolcat
+echo "99.Exit"
+echo
+ read -p "Pilih Nomor Bangast :" pill
+   
+
+  if [ $pill = '1' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+  if [ $pill = '2' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+fi
+
+  if [ $pill = '3' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/BlackCyberAnonim/B-VIRUS
+cd B-VIRUS
+python2 B-VIRUS.py
+fi
+
+  if [ $pill = '4' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+
+  if [ $pill = '5' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/storiku/darkfb
+cd darkfb
+python2 Dark.py
+fi
+
+  if [ $pill = '6' ]
+  then
+
+pkg install git
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+  if [ $pill = '7' ]
+  then
+
+pkg install git
+git clone https://github.com/storiku/instahack
+cd instahack
+bash ig.sh
+fi
+
+  if [ $pill = '8' ]
+  then
+
+pkg install git
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py
+fi
+
+  if [ $pill = '9' ]
+  then
+
+pkg update && apt upgrade -y
+pkg install git -y
+pkg install tor -y
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+fi
+
+  if [ $pill = '10' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod +x weeman.py
+python2 weeman.py
+fi
+
+  if [ $pill = '11' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/storiku/HackCCTV
+cd HackCCTV
+python2 HCCTV.py
+fi
+
+  if [ $pill = '12' ]
+  then
+
+clear
+pkg install php
+pkg install curl
+pkg install git
+git clone https://github.com/kumpulanremaja/pp-fb
+cd pp-fb
+php fb.php
+fi
+
+
+  if [ $pill = '13' ]
+  then
+
+clear
+pkg install git
+pkg install figlet
+git clone https://github.com/ClayHackerTeam/Prankers
+cd Prankers
+sh Prankers.sh
+fi
+
+  if [ $pill = '14' ]
+  then
+
+pkg install git
+pkg install openssh
+pkg install bash
+cd /sdcard
+git clone https://github.com/thelinuxchoice/saycheese
+mkdir hackcam
+cd hackcam
+cd saycheese
+bash saycheese.sh
+fi
+
+  if [ $pill = '15' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/Hax4us/Metasploit_termux
+cd Metasploit_termux
+chmod +x metasploit.sh
+./metasploit.sh
+fi
+
+  if [ $pill = '16' ]
+  then
+
+apt install git -y
+apt install python2 -y
+git clone https://github.com/CiKu370/ko-dork
+cd ko-dork
+python2 dork.py
+fi
+
+  if [ $pill = '17' ]
+  then
+
+pkg install git
+git clone https://github.com/BlackCyber091/v1
+cd v1
+sh v1.sh
+fi
+
+  if [ $pill = '18' ]
+  then
+
+clear
+pkg install git
+git clone https://www.github.com/NeloF4/brutegmail.git
+cd brutegmail
+python2 Hellow.py
+fi
+
+  if [ $pill = '19' ]
+   then
+
+apt update && apt upgrade -y
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install curl
+pkg install wget
+pkg install git
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/SSH/Apt/ssh-apt.sh && bash ssh-apt.sh
+start-kali.sh
+ ./start-kali.sh
+fi
+
+  if [ $pill = '20' ]
+   then
+pkg install git
+pkg install python2
+git clone https://github.com/storiku/darkfb
+cd darkfb
+python2 Dark.py
+fi
+
+  if [ $pill = '21' ]
+   then
+
+pkg update
+pkg upgrade
+pkg install git
+pkg install wget
+pkg install proot
+git clone https://github.com/BlackCyber091/ubuntu
+cd ubuntu
+chmod +x ubuntu.sh
+./ubuntu.sh
+fi
+
+  if [ $pill = '22' ]
+   then
+
+pkg install git
+pkg install python2
+git clone https://github.com/karjok/Terkey
+cd Terkey
+chmod +x terkey.py
+python2 terkey.py
+fi
+
+
+  if [ $pill = '23' ]
+   then
+
+pkg install git
+pkg install python
+pkg install bash
+pkg install curl
+git clone https://github.com/kumpulanremaja/tembakxl
+cd tembakxl
+bash dor-xl.sh
+fi
+
+
+  if [ $pill = '24' ]
+   then
+
+pkg install bash
+pkg install curl
+pkg install git
+git clone https://github.com/Rusmana-ID/tembak-three
+cd tembak-three
+bash three.sh
+fi
+
+  if [ $pill = '25' ]
+   then
+
+pkg install git
+git clone https://github.com/BlackCyber091/TeamDrcyber
+cd TeamDrcyber
+sh Team.sh
+fi
+
+   if [ $pill = '26' ]
+   then
+
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+  if [ $pill = '27' ]
+   then
+
+apt install git
+apt install curl
+apt install figlet
+apt install ruby
+gem install lolcat
+git clone https://github.com/Mr-xDODOL/VIRTEX
+cd VIRTEX
+chmod +x virtex.sh
+sh virtex.sh
+fi
+
+  if [ $pill = '28' ]
+   then
+
+pkg install git
+git clone https://github.com/BlackCyber091/botjones
+cd botjones
+sh cinta.sh
+fi
+
+   if [ $pill = '29' ]
+   then
+
+
+echo $red'Belum Buat Njeng'
+fi
+
+   if [ $pill = '30' ]
+   then
+
+echo '  https://chat.whatsapp.com/LYVWZcliV22HtjzPTPUEo6'
+echo 'salin Aja Link Diatas Lalu Tempel Ke Gogle'
+fi
+
+
+
+  if [ $pill = '31' ]
+   then
+
+pkg install python2
+pkg install git
+git clone https://github.com/pirmansx/mbf
+cd mbf
+chmod +x MBF.py
+python2 MBF.py
+fi
+
+  if [ $pill = '32' ]
+   then
+
+pkg install git
+git clone https://github.com/reyspeed/Tools-V1
+cd Tools-V1
+sh tools.sh
+fi
+
+  if [ $pill = '33' ]
+   then
+
+pkg install php
+pkg install git
+git clone https://github.com/BlackCyber091/bmkg
+cd bmkg
+php bmkg.php
+fi
+
+  if [ $pill = '34' ]
+   then
+
+echo " Orang Sabar Di Sayang Tuhan "| lolcat
+sleep 1
+apt update && apt upgrade
+apt install wget
+mkdir ngrok
+cd ~/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+mv ngrok-stable-linux-arm.zip $HOME
+$HOME/unzip ngrok-stable-linux-arm.zip
+cd ~/
+echo " Done Install Cuk "| lolcat
+fi
+
+  if [ $pill = '35' ]
+   then
+
+echo " Sabar Bujang "| lolcat
+sleep 1
+pkg install git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+echo " Done Install Njeng "| lolcat
+fi
+
+  if [ $pill = '36' ]
+   then
+
+echo " Sabar Njeng "| lolcat
+sleep 1
+pkg install python2
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+echo " Done Install Bujang "| lolcat
+fi
+
+  if [ $pill = '37' ]
+   then
+
+echo " Installing wifi-hacker "| lolcat
+sleep 1
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker/
+chmod +x wifi-hacker.sh
+./wifi-hacker.sh
+echo " Done Install Cuk "| lolcat
+fi
+
+  if [ $pill = '38' ]
+   then
+
+clear
+echo " Installing lazymux "| lolcat
+pkg update && upgrade
+pkg install python2
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+chmod +x lazymux.py
+python2 lazymux.py
+fi
+
+
+  if [ $pill = '39' ]
+   then
+
+echo " Installing whatshack "| lolcat
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+pkg install curl
+git clone https://github.com/AndriGanz/whatshack
+cd whatshack
+ls
+sh whatshack.sh
+fi
+
+
+  if [ $pill = '40' ]
+   then
+
+echo " Installing katoolin "| lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install git python2 gnupg nano
+git clone https://github.com/LionSec/katoolin
+cd katoolin
+python2 katoolin.py
+echo " Done Install Cuk "| lolcat
+fi
+
+  if [ $pill = '41' ]
+   then
+
+echo " Installing VulnScaner "| lolcat
+sleep 1
+apt update
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/kereh/VulnScaner
+mv VulnScaner $HOME
+cd $HOME/VulnScaner
+python2 VulnScaner.py
+fi
+
+
+  if [ $pill = '42' ]
+   then
+
+
+echo " Installing fedora "| lolcat
+sleep 1
+apt update && apt upgrade
+apt-get install git
+apt install wget
+git clone https://github.com/nmilosev/termux-fedora.git
+mv termux-fedora
+cd /termux-fedora
+chmod +x termux-fedora.sh
+sh termux-fedora.sh
+fi
+
+
+  if [ $pill = '43' ]
+   then
+
+echo " Installing IPGeolocation "| lolcat
+sleep 1
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+echo " Done Install Cuk "| lolcat
+fi
+
+  if [ $pill = '44' ]
+   then
+
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+fi
+
+  if [ $pill = '45' ]
+   then
+
+pkg update && pkg upgrade
+pkg install php git
+git clone https://github.com/alintamvanz/diejoubu
+cd diejoubu
+cd v1.2
+php diejoubu.php
+fi
+
+  if [ $pill = '46' ]
+   then
+
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Senitopeng/BotFbBangDjon.git
+cd BotFbBangDjon
+python2 bangdjon.py
+fi
+
+  if [ $pill = '47' ]
+   then
+
+pkg install python2
+pkg install git
+git clone https://github.com/viruz09/CreaterVirus
+cd CreaterVirus
+python2 creater.py
+fi
+
+  if [ $pill = '48' ]
+   then
+
+pkg install python
+pkg install git
+pkg install php
+git clone https://github.com/Cvar1984/pemulungBTC.git
+mv pemulungBTC $HOME
+cd $HOME/pemulungBTC
+php autoload.php
+php mulung.php
+fi
+
+  if [ $pill = '49' ]
+   then
+
+pkg update && pkg upgrade -y
+pkg install python2 -y
+pkg install apache2
+pkg install php -y
+pkg install git
+git clone https://github.com/Senitopeng/PhisingGame
+cd PhisingGame
+python2 phising.py
+fi
+
+  if [ $pill = '50' ]
+   then
+
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/AdheBolo/AdminFinder
+ls
+cd AdminFinder
+chmod 777 AdminFinder.py
+python2 AdminFinder.py
+fi
+
+  if [ $pill = '99' ]
+   then
+
+echo " Kalau Ada Keslahan / Eror "| lolcat
+sleep 1
+echo " Hubungi"| lolcat
+sleep 1
+echo " 08989956316 "| lolcat
+sleep 1
+echo " Datang Lagi Ya  :* "| lolcat
+sleep 1
+figlet "*_*" | lolcat
+exit
+fi
